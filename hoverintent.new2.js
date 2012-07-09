05d63372126217e059b0cfac4155ea6fb8af33ec
Tim Hettler <me@timhettler.com>
2012-07-09 15:48:50 -0400

    DRYing things up a bit

--- hoverintent.new2.js
+++ hoverintent.new2.js
@@ -1,16 +1,16 @@
 (function ($) {
 
-    var default_configuration = {
-        sensitivity: 7,
-        inTimeout: 150,
-        outTimeout: 150
+    $.hoverDelayOptions = {
+        mouseover : 150,
+        mouseout : 150
     };
 
     $.event.special.mouseover = $.event.special.mouseout = {
 
         setup: function () {
             $(this)
-                .data('hoverIntent', {
+                .data('hoverDelay', {
+                    state : 0,
                     timer_id : 0
                 });
 
@@ -19,7 +19,7 @@
 
         teardown: function () {
             $(this)
-                .removeData('hoverIntent');
+                .removeData('hoverDelay');
         },
 
         add: function ( handleObj ) {
@@ -33,40 +33,22 @@
                 if ( this !== event.target ) { return; }
 
                 var $self = $(this),
-                    data = $self.data('hoverIntent'),
-                    delay = event.data;
+                    data = $self.data('hoverDelay'),
+                    nextState = (event.type === 'mouseover') ? 1 : 0;
+                    delay = (typeof event.data === 'number') ? event.data : $.hoverDelayOptions[event.type],
 
-                if (event.type === 'mouseover') {
+                data.timer_id = clearTimeout(data.timer_id);
 
-                    data.timer_id = clearTimeout(data.timer_id);
+                if (data.state !== nextState) {
 
-                    if (data.state !== 1) {
+                    data.timer_id = (function(obj, args) { return setTimeout(
+                        function(){
+                            data.state = nextState;
+                            old_handler.apply(obj, args);
+                        },
+                        delay
+                    );})(this, arguments);
 
-                        data.timer_id = (function(obj, handler, args) { return setTimeout(
-                            function(){
-                                data.state = 1;
-                                old_handler.apply(obj, args);
-                            },
-                            delay
-                        );})(this, old_handler, arguments);
-
-                    }
-
-                } else {
-
-                    data.timer_id = clearTimeout(data.timer_id);
-
-                    if (data.state === 1) {
-
-                        data.timer_id = (function(obj, handler, args) { return setTimeout(
-                            function(){
-                                data.state = 0;
-                                handler.apply(obj, args);
-                            },
-                            delay
-                        );})(this, old_handler, arguments);
-
-                    }
                 }
             }
         }
